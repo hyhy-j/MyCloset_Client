90a0a72eb724237f92a372231a36aea6bc0a4872
hyhy-j <lhjlhj10088@gmail.com>
2025-10-30 15:28:08 +0900

    [Settings] 프로젝트 기본 세팅

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <div data-device>d</div>
+  )
+}
+
+export default App
---
+++ src/pages/Intro.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from "styled-components";
+
+const IntroContainer = styled.div`
+
+`
+
+const Intro = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Intro;
